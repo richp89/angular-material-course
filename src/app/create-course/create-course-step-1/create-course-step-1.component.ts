5dc2ab9f11b40f9420744477dcf76526c98a012a
richp89 <richp89@gmail.com>
2023-12-02 22:54:10 -0500

    Worked on radio buttons, select box, date picker, check boxes, and text area.

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,8 @@
 import { Component } from "@angular/core";
 import { UntypedFormBuilder, Validators } from "@angular/forms";
+import { MatCalendarCellClassFunction } from "@angular/material/datepicker";
+
+const SAMPLE_TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
 @Component({
   selector: "create-course-step-1",
@@ -12,12 +15,25 @@ export class CreateCourseStep1Component {
       "",
       [Validators.required, Validators.minLength(5), Validators.maxLength(60)],
     ],
-    releasedAt: [new Date(), Validators.required],
+    releasedAt: [new Date(1990, 0, 1), Validators.required],
     category: ["BEGINNER", Validators.required],
     courseType: ["premium", Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ["", [Validators.required, Validators.minLength(3)]],
+    longDescription: [
+      SAMPLE_TEXT,
+      [Validators.required, Validators.minLength(3)],
+    ],
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate();
+
+    if (view == "month") {
+      return date == 1 ? "higlight-date" : "";
+    }
+
+    return "";
+  };
 
   constructor(private fb: UntypedFormBuilder) {}
 
