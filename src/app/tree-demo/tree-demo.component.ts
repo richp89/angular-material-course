51b1fc52f50c67ba39f24aa5b104f74cfa8a50ad
richp89 <richp89@gmail.com>
2023-12-10 20:48:19 -0500

    Completed mat tree.

--- src/app/tree-demo/tree-demo.component.ts
+++ src/app/tree-demo/tree-demo.component.ts
@@ -11,6 +11,12 @@ interface CourseNode {
   children?: CourseNode[];
 }
 
+interface CourseFlatNode {
+  name: string;
+  expandable: boolean;
+  level: number;
+}
+
 const TREE_DATA: CourseNode[] = [
   {
     name: "Angular For Beginners",
@@ -67,11 +73,39 @@ export class TreeDemoComponent implements OnInit {
     (node) => node.children
   );
 
+  flatTreeControl = new FlatTreeControl<CourseFlatNode>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    (node: CourseNode, level: number): CourseFlatNode => {
+      return {
+        name: node.name,
+        expandable: node.children?.length > 0,
+        level,
+      };
+    },
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+
+  flatDataSource = new MatTreeFlatDataSource(
+    this.flatTreeControl,
+    this.treeFlattener
+  );
+
   ngOnInit() {
     this.nestedDataSource.data = TREE_DATA;
+    this.flatDataSource.data = TREE_DATA;
   }
 
   hasNestedChild(index: number, node: CourseNode) {
     return node?.children?.length > 0;
+  }
+
+  hasFlatChild(index: number, node: CourseFlatNode) {
+    return node.expandable;
   }
 }
