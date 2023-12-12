2f69324d206cb1eb5b2fec6dfdd591d46c0055d8
richp89 <richp89@gmail.com>
2023-12-11 21:09:39 -0500

    Completed course,.

--- src/app/virtual-scrolling/virtual-scrolling.component.ts
+++ src/app/virtual-scrolling/virtual-scrolling.component.ts
@@ -1,18 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "virtual-scrolling",
-  templateUrl: 'virtual-scrolling.component.html',
-  styleUrls: ["virtual-scrolling.component.scss"]
+  templateUrl: "virtual-scrolling.component.html",
+  styleUrls: ["virtual-scrolling.component.scss"],
 })
 export class VirtualScrollingComponent implements OnInit {
+  items = Array.from({ length: 100000 }).map((value, i) => `Item #${i}`);
 
-  items = Array.from({length: 100}).map((value, i) => `Item #${i}`);
-
-  ngOnInit() {
-
-  }
-
-
+  ngOnInit() {}
 }
